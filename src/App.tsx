703729ac5f074e485e03059bde8e48626285e327
IndalAwalaikal <indalawalaikal05@gmail.com>
2026-02-18 15:57:29 +0800

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import PublicLayout from "./components/PublicLayout";
 import Index from "./pages/Index";
 import Events from "./pages/Events";
@@ -10,6 +11,7 @@ import EventDetail from "./pages/EventDetail";
 import Register from "./pages/Register";
 import DocumentationPage from "./pages/DocumentationPage";
 import Contact from "./pages/Contact";
+
 import AdminLogin from "./pages/admin/AdminLogin";
 import AdminLayout from "./pages/admin/AdminLayout";
 import AdminDashboard from "./pages/admin/AdminDashboard";
@@ -17,6 +19,7 @@ import AdminEvents from "./pages/admin/AdminEvents";
 import AdminRegistrants from "./pages/admin/AdminRegistrants";
 import AdminDocumentations from "./pages/admin/AdminDocumentations";
 import AdminPosters from "./pages/admin/AdminPosters";
+
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -28,6 +31,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          {/* Public Routes */}
           <Route element={<PublicLayout />}>
             <Route path="/" element={<Index />} />
             <Route path="/events" element={<Events />} />
@@ -37,16 +41,34 @@ const App = () => (
             <Route path="/contact" element={<Contact />} />
           </Route>
 
+          {/* Admin Login */}
           <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<NotFound />} />
-            <Route path="dashboard" element={<AdminDashboard />} />
-            <Route path="events" element={<AdminEvents />} />
-            <Route path="registrants" element={<AdminRegistrants />} />
-            <Route path="documentations" element={<AdminDocumentations />} />
-            <Route path="posters" element={<AdminPosters />} />
+
+          {/* Admin Pages with Layout */}
+          <Route path="/admin/dashboard" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
           </Route>
 
+          <Route path="/admin/events" element={<AdminLayout />}>
+            <Route index element={<AdminEvents />} />
+          </Route>
+
+          <Route path="/admin/registrants" element={<AdminLayout />}>
+            <Route index element={<AdminRegistrants />} />
+          </Route>
+
+          <Route path="/admin/documentations" element={<AdminLayout />}>
+            <Route index element={<AdminDocumentations />} />
+          </Route>
+
+          <Route path="/admin/posters" element={<AdminLayout />}>
+            <Route index element={<AdminPosters />} />
+          </Route>
+
+          {/* /admin root → NotFound */}
+          <Route path="/admin" element={<NotFound />} />
+
+          {/* Catch All */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
